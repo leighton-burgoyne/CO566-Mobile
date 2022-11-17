1f2d161180b6e1594688883517ec23b839a78695
Leighton Burgoyne <91318744+leighton-burgoyne@users.noreply.github.com>
2022-11-17 17:02:43 +0000

    Started App Structure

---
+++ AssignmentTrackerApp/AssignmentTrackerApp/AssignmentTrackerApp/AppShell.xaml.cs
@@ -0,0 +1,16 @@
+﻿using AssignmentTrackerApp.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AssignmentTrackerApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ AssignmentTrackerApp/AssignmentTrackerApp/AssignmentTrackerApp/Views/AssignmentsPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AssignmentTrackerApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AssignmentsPage : ContentPage
+    {
+        string _userData = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "data.txt");
+        public AssignmentsPage()
+        {
+            InitializeComponent();
+
+            if (File.Exists(_userData))
+            {
+                editor.Text = File.ReadAllText(_userData);
+            }
+        }
+
+        void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            File.WriteAllText(_userData, editor.Text);
+        }
+        void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            if (File.Exists(_userData))
+            {
+                File.Delete(_userData);
+            }
+            editor.Text = string.Empty;
+        }
+    }
+}
